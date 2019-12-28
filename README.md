79d3f36bd39151ce38eff7ad03944ed13b471b5f
Lee kwangsung <igwangseong@LEEui-MacBook-Pro.local>
2019-12-28 20:58:42 +0900

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # prismagram
 instagram clone with Express + Prisma + React + React Native
+
+## User Stories 
+- [ ] see the feed 
+- [ ] like / unlike a photo
+- [ ] comment on a photo
+- [ ] search by user
+- [ ] search by location 
+- [ ] see user profile
+- [ ] follow / unfollow user
+- [ ] see the full photo
+- [ ] edit my profile
+- [ ] create account
+- [ ] log in 
+- [ ] upload a photo
+- [ ] edit photo(Delete)
---
+++ src/server.js
@@ -0,0 +1,26 @@
+import dotenv from 'dotenv';
+import path from 'path';
+dotenv.config({ path: path.resolve(__dirname, '.env') });
+import { GraphQLServer } from 'graphql-yoga';
+import logger from 'morgan';
+import schema from './schema';
+import { sendSecretMail } from './utils';
+
+const PORT = process.env.PORT || 4000;
+// const typeDefs = `
+//     type Query {
+//         hello : String!
+//     }
+// `;
+
+// const resolvers = {
+// 	Query: {
+// 		hello: () => 'Hi',
+// 	},
+// };
+
+const server = new GraphQLServer({ schema });
+
+server.express.use(logger('dev'));
+
+server.start({ port: PORT }, () => console.log(`Server running on http://localhost:${PORT}`));
